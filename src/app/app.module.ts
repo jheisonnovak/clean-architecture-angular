6e3c92c596521617202b283942e79ed6cdbaa728
Jheison Novak <126830678+JheisonNovak@users.noreply.github.com>
2025-05-02 09:56:33 -0300

    feat(app): remove unused app.component.css file; update app.module.ts to include ToastrModule for notifications

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,14 @@
 import { provideHttpClient } from "@angular/common/http";
 import { NgModule } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
+import { ToastrModule } from "ngx-toastr";
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 import { HeaderComponent } from "./shared/header/header.component";
 
 @NgModule({
 	declarations: [AppComponent, HeaderComponent],
-	imports: [BrowserModule, AppRoutingModule],
+	imports: [BrowserModule, AppRoutingModule, ToastrModule.forRoot({ progressAnimation: "increasing" })],
 	providers: [provideHttpClient()],
 	bootstrap: [AppComponent],
 })
