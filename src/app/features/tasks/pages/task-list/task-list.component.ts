1c47f8dfce0642c0eff370b25075a4b96f3d4e49
Jheison Novak <126830678+JheisonNovak@users.noreply.github.com>
2025-05-05 08:43:38 -0300

    feat(tasks): implement delete task functionality with success and error notifications

--- src/app/features/tasks/pages/task-list/task-list.component.ts
+++ src/app/features/tasks/pages/task-list/task-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from "@angular/core";
+import { ToastrService } from "ngx-toastr";
 import { TasksStore } from "../../stores/tasks.store";
 
 @Component({
@@ -7,9 +8,20 @@ import { TasksStore } from "../../stores/tasks.store";
 	standalone: false,
 })
 export class TaskListComponent implements OnInit {
-	constructor(public store: TasksStore) {}
+	constructor(public store: TasksStore, private toastr: ToastrService) {}
 
 	ngOnInit(): void {
 		this.store.fetchTasks();
+	}
+
+	deleteTask(id: number) {
+		this.store.deleteTask(id).subscribe({
+			next: response => {
+				this.toastr.success(response.message);
+			},
+			error: error => {
+				this.toastr.error(error.message);
+			},
+		});
 	}
 }
