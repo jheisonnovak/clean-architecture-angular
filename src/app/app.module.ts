499a9a0afadd31e3e585528cc3c41a11073c72db
Jheison Novak <126830678+JheisonNovak@users.noreply.github.com>
2025-05-05 08:37:46 -0300

    feat(app): add provideAnimationsAsync to AppModule providers for animation support

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { provideHttpClient } from "@angular/common/http";
 import { NgModule } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
+import { provideAnimationsAsync } from "@angular/platform-browser/animations/async";
 import { ToastrModule } from "ngx-toastr";
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
@@ -9,7 +10,7 @@ import { HeaderComponent } from "./shared/header/header.component";
 @NgModule({
 	declarations: [AppComponent, HeaderComponent],
 	imports: [BrowserModule, AppRoutingModule, ToastrModule.forRoot({ progressAnimation: "increasing" })],
-	providers: [provideHttpClient()],
+	providers: [provideHttpClient(), provideAnimationsAsync()],
 	bootstrap: [AppComponent],
 })
 export class AppModule {}
