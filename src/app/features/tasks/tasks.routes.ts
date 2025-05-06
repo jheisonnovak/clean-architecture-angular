1201602eeec02f2d71a7352a52d596b88d5bcb88
Jheison Novak <126830678+JheisonNovak@users.noreply.github.com>
2025-05-06 11:09:50 -0300

    feat(tasks): remove task form route from task routes

--- src/app/features/tasks/tasks.routes.ts
+++ src/app/features/tasks/tasks.routes.ts
@@ -1,14 +1,9 @@
 import { Routes } from "@angular/router";
-import { TaskFormComponent } from "./pages/task-form/task-form.component";
 import { TaskListComponent } from "./pages/task-list/task-list.component";
 
 export const taskRoutes: Routes = [
 	{
 		path: "",
 		component: TaskListComponent,
-	},
-	{
-		path: "create",
-		component: TaskFormComponent,
 	},
 ];
